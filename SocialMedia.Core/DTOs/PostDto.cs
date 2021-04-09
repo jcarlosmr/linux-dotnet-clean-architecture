e1a40f8b39ed4618f7622edff2a6d7095d1a18a8
Juan Carlos Mendoza R <jcarlosmr@gmail.com>
2021-04-09 01:47:11 -0500

    Setting Date propertity to accept null values

--- SocialMedia.Core/DTOs/PostDto.cs
+++ SocialMedia.Core/DTOs/PostDto.cs
@@ -1,12 +1,12 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 
-namespace SocialMedia.Core.DTOs {
-  public class PostDto {
+namespace SocialMedia.Core.DTOs
+{
+  public class PostDto
+  {
     public int PostId { get; set; }
     public int UserId { get; set; }
-    public DateTime Date { get; set; }
-    [Required]
+    public DateTime? Date { get; set; } // El simbolo ? convierte la propiedad en opcional, puede ser nula
     public string Description { get; set; }
     public string Image { get; set; }
   }
